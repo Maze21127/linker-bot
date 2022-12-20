00ec79484f13ee5706f89163994a9a2c21d9b95b
maze <invoker322sf@gmail.com>
2022-12-20 21:06:48 +1000

    add 403 for link

--- telegram/utils/api/__init__.py
+++ telegram/utils/api/__init__.py
@@ -48,6 +48,7 @@ async def create_personal_link(source: str, link: str, domain: str):
                 else LINK_STATUSES['error']
     if status is LinkStatus.SUCCESS:
         pass
+    print(status)
     return status
 
 
